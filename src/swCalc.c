f791f38792bffb2b84d3f7ff3558b01f1a63a3f8
Francois Gauthier <francois.gauthier@ensimag.grenoble-inp.fr>
2017-03-01 10:59:32 +0100

    meuh

--- src/swCalc.c
+++ src/swCalc.c
@@ -8,7 +8,7 @@ struct matrix *swInitMat(char* s1, char *s2){
     m->w = strlen(s2);
     m->cells = mallocOrDie(m->w * m->h * sizeof(struct cell), "alloc cellules");
     for(unsigned int i = 0; i < m->h; i++){
-        for(unsigned int j = 0; j< m->h; j++){
+        for(unsigned int j = 0; j< m->w; j++){
             m->cells[m->w*i + j].score = 0;
             m->cells[m->w*i + j].prevs = 0;
         }
@@ -21,11 +21,12 @@ void swFreeMat(struct matrix *mat){
     if(mat != NULL){
         if(mat->cells != NULL){
             for(int i = 0; i < mat->h; i++){
-                for(int j = 0; j< mat->h; j++){
+                for(int j = 0; j < mat->w; j++){
                     free(&(mat->cells[mat->w*i + j]));
                 }
-            } 
+            }
         }
+        free(&mat);
     }
 }
 
