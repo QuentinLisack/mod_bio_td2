1a11d5b5bf3305467fcfe274929e6137b1d84b67
Quentin Lisack <quentin.lisack@ensimag.grenoble-inp.fr>
2017-03-01 10:02:38 +0100

    truc

--- src/swCalc.c
+++ src/swCalc.c
@@ -3,22 +3,21 @@
 
 struct matrix *swInitMat(char* s1, char *s2){
     struct matrix *m;
-    m = malloc(sizeof(struct matrix));
+    m = mallocOrDie(sizeof(struct matrix), "alloc matrice");
     m->h = strlen(s1);
     m->w = strlen(s2);
-    m->cells = malloc(m->w * m->h * sizeof(struct cell));
-    for(int i = 0; i < m->h; i++){
-        for(int j = 0; j< m->h; j++){
-            cells[m->w*i + j] = malloc(sizeof(struct cell));
-            cells[m->w*i + j]->score = 0;
-            cells[m->w*i + j]->prevs = 0;
+    m->cells = mallocOrDie(m->w * m->h * sizeof(struct cell), "alloc cellules");
+    for(unsigned int i = 0; i < m->h; i++){
+        for(unsigned int j = 0; j< m->h; j++){
+            m->cells[m->w*i + j].score = 0;
+            m->cells[m->w*i + j].prevs = 0;
         }
     } 
     return m;
 }
 
 void swFreeMat(struct matrix *mat){
-    if(mat != NULL){
+    /*if(mat != NULL){
         if(mat->cells != NULL){
             for(int i = 0; i < m->h; i++){
                 for(int j = 0; j< m->h; j++){
@@ -26,7 +25,7 @@ void swFreeMat(struct matrix *mat){
                 }
             } 
         }
-    }
+    }*/
 }
 
 void swPrintMat(struct matrix *mat) {
