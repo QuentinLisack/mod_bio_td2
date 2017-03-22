da51b5e7fa1a56c0fbafb7141f9ca79b48d224f0
Quentin Lisack <quentin.lisack@ensimag.grenoble-inp.fr>
2017-03-22 09:52:07 +0100

    l'affichage marche

--- src/swOut.c
+++ src/swOut.c
@@ -21,8 +21,8 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
   (*prev)=cell.prevs;
   int *s1_align_int = NULL;
   int *s2_align_int = NULL;
-  s1_align_int = mallocOrDie(strlen(s1)*sizeof(int), "Out 1");
-  s2_align_int = mallocOrDie(strlen(s2)*sizeof(int), "Out 2");
+  s1_align_int = mallocOrDie(strlen(s1)*sizeof(int), "alloc error for s1 align");
+  s2_align_int = mallocOrDie(strlen(s2)*sizeof(int), "alloc error for s2 align");
   
   //initialisation des chaines. 0 signifie que le caractère n'es pas considéré dans la chaine : on n'affichera rien. 
   // 1 signifie qu'il fait partie de la chaine : on affichera le caractère
@@ -35,7 +35,7 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
   	s2_align_int[p] = 0;
   }
   
-  while(cell.score!=0) { // pas du tout sûr!!
+  while(cell.prevs!=0) { // pas du tout sûr!!
     //i_BS=mat-
     if ((*prev)&1) {
       (*i_BS)-=1; 
@@ -62,27 +62,40 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
   int s1_start=(*i_BS); // à modifier quand le code sera bon
   int s2_start=(*j_BS);
 
-  /* affichage des résulatats */
+  //on remplit les séquences à afficher avec une bonne gestion des indel
+  int LENGTH = strlen(s1) < strlen(s2) ? strlen(s2) : strlen(s1);
+  char* s1_print = mallocOrDie(LENGTH*sizeof(char), "alloc error for s1 print");
+  char* s2_print = mallocOrDie(LENGTH*sizeof(char), "alloc error for s2 print");
+
+  int p1 = 0, p2 = 0;
+  for(int p = 0; p < LENGTH; p++){
+    if(s1_align_int[p1] == -1){
+      s1_print[p] = s1[p1];
+      s2_print[p] = '-';
+      p1++;
+    }else if(s2_align_int[p2] == -1){
+      s1_print[p] = '-';
+      s2_print[p] = s2[p2];
+      p2++;
+    }else if(s1_align_int[p1] == 1 && s2_align_int[p2] == 1){
+      s1_print[p] = s1[p1];
+      s2_print[p] = s2[p2];
+      p1++;
+      p2++;
+    }else if(s1_align_int[p1] == 0 && s2_align_int[p2] == 0){
+      break;
+    }else{
+      printf("PROBLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEME\n");
+    }
+  }
+
+  /* affichage des résultats */
   printf("Best score is %.1f, the best-scoring alignments are:\n\n ",(*BestScore));
   printf("s1 alignment starts at coord %d\n s2 alignment starts at coord %d\n\n",s1_start,s2_start);
   printf("les sequences alignees sont : \n");
-  for(unsigned int p = 0; p < strlen(s1); p++){
-  	if(s1_align_int[p] == 1){
-  	  printf("%c", s1[p]);
-  	}
-  	if(s1_align_int[p] == -1){
-  	  printf("-");
-  	}
-  }
-  printf("\n");
-  for(unsigned int p = 0; p < strlen(s2); p++){
-  	if(s2_align_int[p] == 1){
-  	  printf("%c", s2[p]);
-  	}
-  	if(s2_align_int[p] == -1){
-  	  printf("-");
-  	}
-  }
+  printf("%s\n", s1_print);
+  printf("%s\n", s2_print);
+  
   printf("\n");
   printf("les sequences initiales sont : \n");
   printf("s1	%s\n",s1);
@@ -94,6 +107,8 @@ void printBestAlis(struct matrix *mat, struct cost *cost, char *s1, char *s2) {
   free(i_BS);
   free(j_BS);
   free(prev);
+  free(s1_print);
+  free(s2_print);
 }
 
 void Calcul_BestScore(struct matrix *mat, double* BS, unsigned int* iBS, unsigned int* jBS) {
